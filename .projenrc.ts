09b508766c83f742eaed8b0439e25bae23392e80
yicr <yicr@users.noreply.github.com>
2023-08-25 13:00:24 +0900

    chore(deps): jsii version set to ~5.0.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,6 +4,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
   typescriptVersion: '4.9.x',
+  jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-secure-vpc-bucket',
   description: 'Access from specific VPC Endpoint only Bucket',
