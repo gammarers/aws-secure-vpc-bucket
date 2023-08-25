b8d3c0a90d361742bdd25762d0ca6d753b565f27
yicr <yicr@users.noreply.github.com>
2023-08-25 12:57:55 +0900

    chore(deps): typescript version set to 4.9.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,6 +3,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
   cdkVersion: '2.80.0',
+  typescriptVersion: '4.9.x',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-secure-vpc-bucket',
   description: 'Access from specific VPC Endpoint only Bucket',
