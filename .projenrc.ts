fc1134e0932a6cafc975cc66360cd327f8e9d011
yicr <yicr@users.noreply.github.com>
2023-09-15 18:17:05 +0900

    chore: change repository url

--- .projenrc.ts
+++ .projenrc.ts
@@ -10,7 +10,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   description: 'Access from specific VPC Endpoint only Bucket',
   keywords: ['aws', 'cdk', 'aws-cdk', 's3', 'bucket', 'vpc', 'endpoint', 'vpce'],
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/yicr/aws-secure-vpc-bucket.git',
+  repositoryUrl: 'https://github.com/gammarer/aws-secure-vpc-bucket.git',
   npmAccess: javascript.NpmAccess.PUBLIC,
   deps: [
     '@gammarer/aws-secure-bucket@~0.12.4',
